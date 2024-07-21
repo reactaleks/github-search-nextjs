67dad80e0a9159fd3ba05957074b14a33147590d
Aleks <a.ivanov.developer@gmail.com>
2024-07-21 16:37:05 +0100

    Added link to blog, Added underline to account tag and blog link, Converted date joined to correct format

--- components/OutputComponent.tsx
+++ components/OutputComponent.tsx
@@ -1,14 +1,11 @@
 // Things needed from github api
 // Login / Avatar/ Name / Created_at / Bio / Public_repos / Followers / Following / Location / Blog / Twitter_username / Company
-import Image from "next/image";
 import TwitterIconComponent from "@/public/assets/icon-twitter";
 import WebsiteIconComponent from "@/public/assets/icon-website";
 import LocationIconComponent from "@/public/assets/icon-location";
 import CompanyIconComponent from "@/public/assets/icon-company";
 
 export default function OutputComponent(props: any) {
-
-  
   return (
     <div className="w-[327px] h-[517px] rounded-xl shadow-xl bg-white mx-auto grid grid-rows-24 grid-cols-24 dark:bg-darkModeBlue">
       <img
@@ -22,12 +19,12 @@ export default function OutputComponent(props: any) {
         {!props.userData.name ? 'Mystery' : props.userData.name}
       </div>
       <div className="row-span-1 col-span-10 col-start-8 row-start-3 font-space text-[13px]">
-        <a className="text-blue font-space text-[13px]">
+        <a href={props.userData.html_url} target="_blank" className="text-blue font-space text-[13px] cursor-pointer hover:underline">
           @{props.userData.login}
         </a>
       </div>
       <div className="row-span-1 col-span-12 col-start-8 row-start-4 font-space text-[13px] text-lightblue dark:text-darkModeWhite">
-        {props.userData.created_at}
+        Joined {new Date(props.userData.created_at).toLocaleDateString('en-GB', { day: '2-digit', month: 'long', year: 'numeric' })}
       </div>
       <p className={`row-span-4 col-span-20 col-start-3 row-start-6 font-space text-[13px] leading-[25px] ${!props.userData.bio ? 'text-[#A4B4CC] dark:text-[#A4B4CC]' : 'text-lightblue dark:text-darkModeWhite'} `}>
         {!props.userData.bio ? 'I am a misterious github user who has nothing in my bio' : props.userData.bio}
@@ -61,7 +58,7 @@ export default function OutputComponent(props: any) {
       </div>
       <WebsiteIconComponent className={`${!props.userData.blog ? 'fill-[#A4B4CC] dark:fill-[#A4B4CC]'  : 'fill-darkblue'} dark:fill-white  w-[20px] h-[20px] row-span-1 col-span-2 row-start-18 col-start-2`} />
       <div className={`font-space text-[13px] ${!props.userData.blog ? 'text-[#A4B4CC] dark:text-[#A4B4CC]' : 'text-darkblue'}  row-span-1 col-span-18 row-start-18 col-start-4 dark:text-darkModeWhite`}>
-        {!props.userData.blog ? 'Not available' : props.userData.blog}
+         {!props.userData.blog ? 'Not available' : <a href={props.userData.blog} target="_blank" className="cursor-pointer hover:underline">{props.userData.blog}</a>}
       </div>
       <TwitterIconComponent className={`${!props.userData.twitter_username ? 'fill-[#A4B4CC] dark:fill-[#A4B4CC]' : 'fill-darkblue'} dark:fill-white  w-[20px] h-[20px] row-span-1 col-span-2 row-start-20 col-start-2`} />
       <div className={`font-space text-[13px] ${!props.userData.twitter_username ? 'text-[#A4B4CC] dark:text-[#A4B4CC]' : 'text-darkblue'}  row-span-1 col-span-18 row-start-20 col-start-4 dark:text-darkModeWhite`}>
